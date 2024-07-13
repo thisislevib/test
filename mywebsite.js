8c3470d20fe268437a89fc6fa2ab7f8c9db18ab9
Levi Boigeol <162833814+thisislevib@users.noreply.github.com>
2024-07-13 23:03:24 +0300

    Add files via upload

---
+++ mywebsite.js
@@ -0,0 +1,36 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
+
+let answers = document.querySelectorAll(".accordion");
+answers.forEach((event) => {
+    event.addEventListener("click", () => {
+        if (event.classList.contains("active")) {
+            event.classList.remove("active");
+        } else {
+            event.classList.add("active");
+        }
+    });
+});
